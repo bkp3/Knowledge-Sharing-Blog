e3ee29b4233b8a78b36f799dd71009468dadd540
bkp3 <bholakumar.7372@gmail.com>
2021-05-17 07:42:52 +0530

    updated...1.3

--- src/java/com/blog/dao/PostDao.java
+++ src/java/com/blog/dao/PostDao.java
@@ -1,5 +1,21 @@
 package com.blog.dao;
+import com.blog.entities.Category;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class PostDao {
+    
+    Connection con;
+    
+    public PostDao(Connection con){
+        this.con=con;
+    }
+    
+    public ArrayList<Category>getAllCategory(){
+        ArrayList<Category> list=null;
+        
+        
+        return null;
+    }
     
 }
